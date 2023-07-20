81c2a67cbf27d4fce72fbdf9ad5b6d6dabf0a768
emanuil-staello <94181307+emanuil-staello@users.noreply.github.com>
2023-07-20 23:22:58 +0300

    Update EventItem.tsx

--- src/components/EventItem/EventItem.tsx
+++ src/components/EventItem/EventItem.tsx
@@ -27,6 +27,7 @@ export const EventItem = ({ onPress, ...rest }: EventItemProps) => {
       <View>
         <Text style={textStyles.title}>{name}</Text>
         <Text style={textStyles.subtitle}>{date}</Text>
+        <Text style={textStyles.subtitle}>'Banica'</Text>
         <Text style={textStyles.subtitle}>{service}</Text>
       </View>
     </TouchableOpacity>
