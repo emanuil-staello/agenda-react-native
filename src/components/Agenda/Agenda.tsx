dab84d8a55167922a53080674f367ea4df6cd171
emanuil-staello <94181307+emanuil-staello@users.noreply.github.com>
2023-07-21 15:45:15 +0300

    Update Agenda.tsx

--- src/components/Agenda/Agenda.tsx
+++ src/components/Agenda/Agenda.tsx
@@ -67,6 +67,7 @@ export const Agenda = ({
             locale={locale}
             firstDayMonday={firstDayMonday}
             theme={monthTheme}
+            re_render_elements={re_render_elements}
           />
         </View>
       )}
@@ -78,6 +79,7 @@ export const Agenda = ({
           locale={locale}
           markedDays={markedDays}
           onPress={onDayPressCallback}
+          re_render_elements={re_render_elements}
         />
       )}
       {changeView}
